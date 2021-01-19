fe91dee11ae98f36dd03d0fd71d0e2cfbaa07284
oncosmin <oncescu.cosmin@gmail.com>
2021-01-19 20:34:00 +0200

    add stylesheet for all qTableWidgets from the application

--- ui_styles.py
+++ ui_styles.py
@@ -49,3 +49,76 @@ class Style():
     }
     """
     )
+    
+    style_table_standard=(
+    """
+    QTableWidget {	
+        background-color: rgb(40, 42, 62);
+        padding: 15px;;
+    }
+    QTableWidget::item{
+        padding-left: 5px;
+        padding-right: 5px;
+        border: 0.5px solid rgb(35, 35, 45);
+    }
+    QTableWidget::item:selected{
+        background-color: rgb(141, 153, 174);
+    }
+    QScrollBar:vertical {
+        background: rgb(43, 45, 66);
+        width: 16px;   
+        margin: 22px 0px 22px 0px;
+	    border-radius: 8px;
+    }
+    QScrollBar::handle:vertical {
+        background: rgb(141, 153, 174);
+        min-width: 16px;
+        border-radius: 8px;
+    }
+    QScrollBar::add-page:vertical {
+        background: rgb(43, 45, 66);
+    }
+    QScrollBar::sub-page:vertical {
+        background: rgb(43, 45, 66);
+    }
+    QScrollBar::add-line:vertical {
+        background: rgb(43, 45, 66);
+        border-bottom-right-radius: 8px;
+        border-bottom-left-radius: 8px;
+        height: 20px;
+        subcontrol-position: bottom;
+        subcontrol-origin: margin;
+    }
+    QScrollBar::sub-line:vertical {
+        background: rgb(43, 45, 66);
+        border-top-right-radius: 8px;
+        border-top-left-radius: 8px;
+        height: 20px;
+        subcontrol-position: top;
+        subcontrol-origin: margin;
+    }
+    QHeaderView::section{
+        background-color: rgb(35, 35, 45);
+        width: 25px;
+        border-style: none;
+    }
+    QTableCornerButton::section{
+        background-color: rgb(35, 35, 45);
+    }
+    QTableWidget::horizontalHeader {	
+        background-color: rgb(35, 35, 45);
+    }
+    QHeaderView::section:horizontal
+    {
+        border: 1px solid rgb(35, 35, 45);
+        background-color: rgb(35, 35, 45);
+        padding: 3px;
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+    }
+    QHeaderView::section:vertical
+    {
+        border: 1px solid rgb(35, 35, 45);
+    }
+    """
+    )
