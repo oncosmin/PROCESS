ec9f9283c4b5963ae2dcf159c627d94b5a0d74fc
oncosmin <oncescu.cosmin@gmail.com>
2021-03-02 15:05:51 +0200

    test file with add input file function

--- test.py
+++ test.py
@@ -1,40 +1,45 @@
-f = open('test.process','r')
+f = open('test2.process','r')
 l = f.readlines()
 lines=[]
 for i in l:
     lines.append(i.strip('\n'))
 
-def writeNastranInputFiles(startCount,lengthCount):
-    for i in range(1,lengthCount):
-        print(lines[startCount+i])
-
-def writeGroupOfElements(startCount,lengthCount):
+def writeFromInputFileToGUI(startCount,lengthCount,cType):
     for i in range(1,lengthCount):
         listLineStr = lines[startCount+i].split(',')
-        listLineNumbers = lines[startCount+i].split('[', 1)[1].split(']')[0]
+        if cType == 1:
+            print(lines[startCount+i])
+        elif cType == 2:
+            listLineNumbers = lines[startCount+i].split('[', 1)[1].split(']')[0]
+            print(listLineStr[0])
+            print('Elm '+listLineNumbers.replace(',',''))
+        elif cType == 3:
+            print(listLineStr[0])
+            print(listLineStr[1])
+        elif cType == 4:
+            print(listLineStr[0])
+        elif cType == 5:
+            print(listLineStr[0])
         
-        print(listLineStr[0])
-        print([int(s) for s in listLineNumbers.split(',')])
-
 for count, line in enumerate(lines):
     #print(count,line)
     if line.startswith('# NASTRAN INPUT/OUTPUT FILES:'):
         a=count
     elif line.startswith('# GROUPS OF ELEMENTS INPUT:'):
         b=count
-        writeNastranInputFiles(a,b-a)
+        writeFromInputFileToGUI(a,b-a,1)
     elif line.startswith('# COMPOSITE MATERIAL FACING INPUT:'):
         c=count
-        writeGroupOfElements(b,c-b)
+        writeFromInputFileToGUI(b,c-b,2)
     elif line.startswith('# COMPOSITE MATERIAL CORE INPUT:'):
         d=count
-        writeCompositeMatFacing(c,d-c)
+        writeFromInputFileToGUI(c,d-c,3)
     elif line.startswith('# METALLIC MATERIAL INPUT:'):
         e=count
-        writeCompositeMatCore(d,e-d)
+        writeFromInputFileToGUI(d,e-d,4)
     elif line.startswith('# END'):
         f=count
-        writeMetallicMat(e,f-e)
+        writeFromInputFileToGUI(e,f-e,5)
     else:
         pass    
 
